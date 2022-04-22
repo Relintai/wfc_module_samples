f72ef4a1f764b5128bb25dd31504be586187dbcd
Relintai <relintai@protonmail.com>
2022-04-23 00:47:41 +0200

    Simple initial test setup.

--- project/TextureRect.gd
+++ project/TextureRect.gd
@@ -1,16 +1,36 @@
 extends TextureRect
 
+export(Image) var img : Image
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var indexer : ImageIndexer = ImageIndexer.new()
+	
+	indexer.index_image(img)
+	
+	var indices : PoolIntArray = indexer.get_color_indices()
+	
+	#<overlapping name="Mazelike" N="3" periodic="True"/>
+	
+	var wfc : OverlappingWaveFormCollapse = OverlappingWaveFormCollapse.new()
+	wfc.periodic_input = true
+	wfc.out_height = 300
+	wfc.out_width = 300
+	wfc.pattern_size = 3
+	wfc.set_input(indices, img.get_width(), img.get_height())
+	wfc.initialize()
+	var res : PoolIntArray = wfc.generate_image_index_data()
+	
+	var data : PoolByteArray = indexer.indices_to_argb8_data(res)
+	
+	var res_img : Image = Image.new()
+	res_img.create_from_data(300, 300, false, Image.FORMAT_RGBA8, data)
+	
+	var res_tex : ImageTexture = ImageTexture.new();
+	res_tex.create_from_image(res_img, 0)
+	
+	texture = res_tex
+
+	
+	
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
